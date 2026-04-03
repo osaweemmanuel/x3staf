4c215c7685278996cb856d5e30bc30315c22b167
osaweemmanuel <emmanuellucky25@gmail.com>
2026-04-03 14:31:49 +0100

    V3: Payload safety guards and final branding fixes

--- SERVER/config/allowedOrigins.js
+++ SERVER/config/allowedOrigins.js
@@ -1,4 +1,3 @@
-//  const allowedOrigins = "http://localhost:5173";
- const allowedOrigins = ['https://www.x3staffinginc.ca', 'https://x3staffinginc.ca','http://localhost:5173'];;
+ const allowedOrigins = ['https://www.x3staffinginc.ca', 'https://x3staffinginc.ca','http://localhost:5173'];
 
 module.exports = allowedOrigins;
